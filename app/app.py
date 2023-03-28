cf4a61ba282704b071bd3b532a165f9307673e1f
Anique Atique Alam <aniq.atiq@gmail.com>
2023-03-28 16:24:35 +0500

    new changes

--- app/app.py
+++ app/app.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/post")
+async def get():
+    return {"data": "This is coming from this post"}
+
+@app.get("/post")
+async def get():
+    return {"data": "This is coming new post"}
